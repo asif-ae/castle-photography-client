817e7a24573d3765eba9d45a958628dacbb3ecf9
Asif Estiak <me.asif.ae@gmail.com>
2021-08-19 15:33:52 +0600

    added important routes

---
+++ src/components/Shared/HeaderTop/HeaderTop.js
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import Brand from '../Brand/Brand';
+
+const HeaderTop = () => {
+  return (
+    <Box>
+      <Brand />
+    </Box>
+  );
+};
+
+export default HeaderTop;
---
+++ src/components/Shared/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <Box>
+      
+    </Box>
+  );
+};
+
+export default Navbar;
