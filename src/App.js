04e0c567c1d0b1bfadac57d62635fe33fc4ad5a1
Asif Estiak <me.asif.ae@gmail.com>
2021-08-19 15:05:53 +0600

    added home and not found component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from './components/Home/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route to="/">
-          {/* <Home /> */}
+        <Route exact to="/">
+          <Home />
         </Route>
-        <Route to="/">
-          {/* <Home /> */}
+        <Route to="*">
+          <NotFound />
         </Route>
       </Switch>
     </Router>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <Typography>
+      Route Not Found
+    </Typography>
+  );
+};
+
+export default NotFound;
