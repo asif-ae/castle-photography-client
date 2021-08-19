839f652915c7788b31544e2821a66071bab04072
Asif Estiak <me.asif.ae@gmail.com>
2021-08-19 14:52:04 +0600

    initialized project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route to="/">
+          {/* <Home /> */}
+        </Route>
+        <Route to="/">
+          {/* <Home /> */}
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
