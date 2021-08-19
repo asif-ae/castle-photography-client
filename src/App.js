f374619f4859f667fe19cb2080c8e572beddb748
Asif Estiak <me.asif.ae@gmail.com>
2021-08-19 15:25:54 +0600

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import Services from './components/Services/Services/Services';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact to="/">
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route to="*">
+        <Route exact path="/services">
+          <Services />
+        </Route>
+        <Route path="*">
           <NotFound />
         </Route>
       </Switch>
